74db47e5f4e74096a0ae06abdda0eaf4fa845731
Christopher Wiedey <c_wied05@uni-muenster.de>
2020-10-07 13:54:24 +0200

    fix

--- src/Navigation.js
+++ src/Navigation.js
@@ -7,6 +7,7 @@ import About from './About';
 import Project from './Project';
 import Card from './react-bootstrap/Card'
 import CardDeck from 'react-bootstrap/CardDeck';
+import Button from 'react-bootstrap/Button';
 
 import projects from './projects.json';
 
